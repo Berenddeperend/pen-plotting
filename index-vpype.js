ed4f02ac57a2d741e15ebffc2ddf7c56b8f7a687
Berend Swennenhuis <berendswennenhuis@gmail.com>
2023-10-16 23:53:42 +0200

    Wippiewip

--- index-vpype.js
+++ index-vpype.js
@@ -1,5 +1,6 @@
 async function createGCode() {
-  const vpype = `vpype --config configs/plotter.toml read svg/myfirstdrawing.svg  penwidth 0.3mm scaleto 19cm 19cm pagesize 20cmx20cm gwrite -p plotter gcode/myfirstdrawing-optimized.gcode`;
+  const vpype = `vpype --config configs/plotter.toml read svg/myfirstdrawing.svg linemerge linesort penwidth 0.3mm scaleto 19cm 19cm pagesize 20cmx20cm gwrite -p plotter gcode/myfirstdrawing-optimized.gcode`;
+  // const vpype = `vpype --config configs/plotter.toml read svg/myfirstdrawing.svg penwidth 0.3mm scaleto 19cm 19cm pagesize 20cmx20cm write svg/myfirstdrawing-besnaveld.svg`;
 
   const stl = await Bun.spawnSync([...vpype.split(" ")]);
   console.log("success", stl.success);
