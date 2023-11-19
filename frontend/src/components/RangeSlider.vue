adcb95e5a814cef806ebcbec3c75452065d5313b
Berend Swennenhuis <berendswennenhuis@gmail.com>
2023-11-19 17:46:04 +0100

    Enable range values

--- frontend/src/components/RangeSlider.vue
+++ frontend/src/components/RangeSlider.vue
@@ -1,21 +1,22 @@
 <script setup>
-defineProps({ modelValue: Number, name: String });
+const props = defineProps({ modelValue: Object, name: String });
 const emit = defineEmits(["update:model-value"]);
 
+const { min, max } = props.modelValue;
+
 const update = (value) => {
-  console.log(value);
-  emit("update:model-value", parseInt(value));
+  emit("update:model-value", { min, max, value: parseInt(value) });
 };
 </script>
 
 <template>
   <!--  <input type="range" v-model="modelValue" :min="0" :max="300" />-->
-  <pre>{{ name }}, {{ modelValue }}</pre>
+  <pre>{{ name }}, {{ props.modelValue.value }}</pre>
   <input
     type="range"
-    :model-value="modelValue"
+    :value="props.modelValue.value"
     @input="update($event.target.value)"
-    :min="0"
-    :max="300"
+    :min="min"
+    :max="max"
   />
 </template>
--- frontend/src/drawings/lines.js
+++ frontend/src/drawings/lines.js
@@ -1,10 +1,25 @@
 import { ref, toRaw, unref } from "vue";
 
+const toMMV = (min, max, value) => {
+  return {
+    min,
+    max,
+    value: value ? value : parseInt(min + max / 2),
+  };
+};
+
+const toValues = (mmvObj) => {
+  return Object.entries(mmvObj).reduce((acc, [key, value]) => {
+    acc[key] = value.value;
+    return acc;
+  }, {});
+};
+
 export default {
   name: "lines",
   // draw: (x = 0, y = 0, count = 10, spacing = 4) => {
   draw: (settings) => {
-    let { x = 0, y = 0, count = 10, spacing = 4 } = toRaw(settings);
+    let { x = 0, y = 0, count = 10, spacing = 4 } = toRaw(toValues(settings));
 
     const lines = [];
     for (let i = 0; i < count; i++) {
@@ -17,10 +32,11 @@ export default {
     }
     return lines.join("\n");
   },
+
   settings: {
-    x: 0,
-    y: 0,
-    count: 10,
-    spacing: 4,
+    x: toMMV(0, 1000),
+    y: toMMV(0, 200),
+    count: toMMV(0, 100),
+    spacing: toMMV(0, 30),
   },
 };
